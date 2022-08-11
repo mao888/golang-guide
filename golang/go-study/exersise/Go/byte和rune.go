0056ff532add3d23a3c76815c5f67cfe29ccba0a
huchao <2557523039@qq.com>
2022-08-11 18:02:56 +0800

    feat:add byte rune

--- golang/go-study/exersise/Go/byte和rune.go
+++ golang/go-study/exersise/Go/byte和rune.go
@@ -4,3 +4,24 @@
     @desc: //TODO
 **/
 package main
+
+import "fmt"
+
+//https://blog.csdn.net/qq_42410605/article/details/114818366
+
+func main() {
+	s := "asc"
+	fmt.Printf("%T  ", s[0])
+	fmt.Println()
+	for i := range s {
+		fmt.Printf("%T ", s[i])
+	}
+	fmt.Println()
+	for _, i2 := range s {
+		fmt.Printf("%T ", i2)
+	}
+	fmt.Println()
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%T ", s[i])
+	}
+}
