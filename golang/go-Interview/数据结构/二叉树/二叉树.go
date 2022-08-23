16290d45733bd375312975a37903fbca0ed92705
huchao <2557523039@qq.com>
2022-08-23 15:51:12 +0800

    feat:add 百度面试

---
+++ golang/go-Interview/数据结构/二叉树/二叉树.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 节点
+type BinaryTreeNode struct {
+	Data  int
+	Left  *BinaryTreeNode
+	Right *BinaryTreeNode
+}
+
+// 创建节点
+func CreateBinaryTree(data int) *BinaryTreeNode {
+	return &BinaryTreeNode{data, nil, nil}
+}
+
+// 插入节点
+func (node *BinaryTreeNode) Insert(n *BinaryTreeNode, data int) bool {
+	cur := n
+	for cur != nil {
+		if cur.Data < data {
+			if cur.Right != nil {
+				cur = cur.Right
+			} else {
+				cur.Right = CreateBinaryTree(data)
+				return true
+			}
+		} else {
+			if cur.Left != nil {
+				cur = cur.Left
+			} else {
+				cur.Left = CreateBinaryTree(data)
+				fmt.Println(data, "d---")
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// 层数打印
+func (node *BinaryTreeNode) BreadthFirstSearch() []int {
+	if node == nil {
+		return nil
+	}
+	var result []int
+	par := node
+	cur := []*BinaryTreeNode{par}
+	for len(cur) > 0 {
+		result = append(result, cur[0].Data)
+		if cur[0].Left != nil {
+			cur = append(cur, cur[0].Left)
+		}
+		if cur[0].Right != nil {
+			cur = append(cur, cur[0].Right)
+		}
+		cur = cur[1:]
+	}
+	return result
+}
+
+// 前序打印
+func (node *BinaryTreeNode) PreOrder(n *BinaryTreeNode) {
+	if n != nil {
+		fmt.Println(n.Data)
+		node.PreOrder(n.Left)
+		node.PreOrder(n.Right)
+	}
+}
+
+// 中序打印
+func (node *BinaryTreeNode) InOrder(n *BinaryTreeNode) {
+	if n != nil {
+		node.InOrder(n.Left)
+		fmt.Println(n.Data)
+		node.InOrder(n.Right)
+	}
+}
+
+// 后序打印
+func (node *BinaryTreeNode) PostOrder(n *BinaryTreeNode) {
+	if n != nil {
+		node.InOrder(n.Left)
+		node.InOrder(n.Right)
+		fmt.Println(n.Data)
+	}
+}
+
+// 获取树的高度
+func (node *BinaryTreeNode) GetHight(n *BinaryTreeNode) int {
+	if n == nil {
+		return 0
+	}
+	l := node.GetHight(n.Left)
+	r := node.GetHight(n.Right)
+	if l > r {
+		return l + 1
+	} else {
+		return r + 1
+	}
+}
+
+// 打印叶子节点
+func (node *BinaryTreeNode) FindLead(n *BinaryTreeNode) {
+	if n != nil {
+		if n.Left == nil && n.Right == nil {
+			fmt.Println(n.Data)
+		}
+		node.FindLead(n.Left)
+		node.FindLead(n.Right)
+	}
+}
+
+// 查找指定值的节点
+func (node *BinaryTreeNode) FindValueNode(n *BinaryTreeNode, target int) *BinaryTreeNode {
+	if n == nil {
+		return nil
+	} else if n.Data == target {
+		return n
+	} else {
+		cur := node.FindValueNode(n.Left, target)
+		if cur != nil {
+			return cur
+		}
+		return node.FindValueNode(n.Right, target)
+	}
+}
+
+func main() {
+	var node *BinaryTreeNode
+	　　 // 创建一个根节点
+	node = CreateBinaryTree(10)
+	li := []int{9, 11, 8, 5, 6, 4, 12, 15, 18, 17}
+	　　 // 创建一个二叉树
+	for _, val := range li {
+		node.Insert(node, val)
+	}
+	ret := node.BreadthFirstSearch()
+	fmt.Println(ret)
+	node.PreOrder(node)
+	node.InOrder(node)
+	node.PostOrder(node)
+	res := node.GetHight(node)
+	fmt.Println(res)
+	node.FindLead(node)
+	ref := node.FindValueNode(node, 17)
+	fmt.Println(ref)
+}
---
+++ golang/go-Interview/笔试/百度-服务体验发展中心_Java工程师实习-面试/baidu.go
@@ -0,0 +1,142 @@
+package main
+
+import "fmt"
+
+//给你一棵二叉树的根节点 root ，二叉树中节点的值 互不相同 。另给你一个整数 start 。在第 0 分钟，感染 将会从值为 start 的节点开始爆发。
+//
+//每分钟，如果节点满足以下全部条件，就会被感染：
+//节点此前还没有感染。
+//节点与一个已感染节点相邻。
+//
+//返回感染整棵树需要的分钟数。
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//
+//(1)
+//start = 3
+//
+//1
+/// \
+//5   3
+///   / \
+//7   10  6
+
+//(2)
+//
+//root = [1], start = 1
+//
+//(3)
+//start = 3
+//
+//1
+//\
+//3
+/// \
+//10  6
+///
+//11
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 创建节点
+func CreateBinaryTree(data int) *TreeNode {
+	return &TreeNode{data, nil, nil}
+}
+
+// 插入节点
+func (node *TreeNode) Insert(n *TreeNode, data int) bool {
+	cur := n
+	for cur != nil {
+		if cur.Val < data {
+			if cur.Right != nil {
+				cur = cur.Right
+			} else {
+				cur.Right = CreateBinaryTree(data)
+				return true
+			}
+		} else {
+			if cur.Left != nil {
+				cur = cur.Left
+			} else {
+				cur.Left = CreateBinaryTree(data)
+				fmt.Println(data, "d---")
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func amountOfTime(root *TreeNode, start int) int {
+	var st *TreeNode
+	parents := map[*TreeNode]*TreeNode{}
+
+	//  dis 求出start的位置和每个点的父节点
+	var dfs func(*TreeNode, *TreeNode)
+	dfs = func(node *TreeNode, pa *TreeNode) {
+		if node == nil {
+			return
+		}
+		if node.Val == start {
+			st = node
+		}
+		parents[node] = pa
+		dfs(node.Left, node)
+		dfs(node.Right, node)
+	}
+	dfs(root, nil)
+
+	ans := -1
+	vis := map[*TreeNode]bool{nil: true, st: true}
+	for q := []*TreeNode{st}; len(q) > 0; ans++ {
+		tmp := q
+		q = nil
+		for _, node := range tmp {
+			if node != nil {
+				if !vis[node.Left] {
+					vis[node.Left] = true
+					q = append(q, node.Left)
+				}
+				if !vis[node.Right] {
+					vis[node.Right] = true
+					q = append(q, node.Right)
+				}
+				if p := parents[node]; !vis[p] {
+					vis[p] = true
+					q = append(q, p)
+				}
+			}
+		}
+	}
+	return ans
+}
+
+func main() {
+	var node *TreeNode
+
+	node = CreateBinaryTree(1)
+	li := []int{5, 3, 7, 10, 6}
+
+	for _, val := range li {
+		node.Insert(node, val)
+	}
+	a := amountOfTime(node, 3)
+	fmt.Println(a)
+}
