4ae6231dd5110dbd5ca9dc050a8409ac079bec22
huchao <2557523039@qq.com>
2022-12-15 20:59:33 +0800

    feat:add一览科技笔试

--- golang/go-Interview/实战/一览科技/Go语言实现并行分段求和计算.go
+++ golang/go-Interview/实战/一览科技/Go语言实现并行分段求和计算.go
@@ -8,15 +8,6 @@ import (
 	"time"
 )
 
-const START = 1
-const LEN = 100
-const STEP = 1000
-
-var (
-	mu  sync.Mutex
-	sum int64
-)
-
 //1.这个程序总的功能是计算1到100000（LEN×STEP）的和，计算被分为STEP步（段）进行，通过调用函数subsum()计算每段之和，然后相加
 //
 //2.函数subsum()计算从start开始长度为len的数列之和
@@ -28,6 +19,15 @@ var (
 //5.使用包"time"中的方法time.Sleep()，让自身的goroutine休眠，代入参数指定休眠0.1秒，因为需要等待所有其他goroutine都执行完之后程序才能结束
 //
 //6.使用互斥锁mu来锁住变量sum（参见程序），需要使用包"sync"
+
+const START = 1
+const LEN = 100
+const STEP = 1000
+
+var (
+	mu  sync.Mutex
+	sum int64
+)
 
 func main() {
 	fmt.Printf("%d\n", (int64(LEN*STEP) * (int64(LEN*STEP) + 1) / int64(2)))
