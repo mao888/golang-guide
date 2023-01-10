1e52bb31ddfdd6bd2334879cb404fda77436d5cb
huchao <2557523039@qq.com>
2023-01-11 00:10:32 +0800

    feat:add 双向链表

--- golang/go-Interview/data-structure/linear_list/link_list/double_linked_list.go
+++ golang/go-Interview/data-structure/linear_list/link_list/double_linked_list.go
@@ -1,144 +1,239 @@
 package linkedlist
 
-import (
-	"fmt"
-)
-
 // 线性表 - 双向链表
-// https://geekr.dev/posts/go-data-structure-linked-list
+// https://studygolang.com/pkgdoc
 
-// 定义节点
-type Node struct {
-	Value    int
-	Previous *Node
-	Next     *Node
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package list implements a doubly linked list.
+//
+// To iterate over a list (where l is a *List):
+//	for e := l.Front(); e != nil; e = e.Next() {
+//		// do something with e.Value
+//	}
+//
+
+// Element is an element of a linked list.
+type Element struct {
+	// Next and previous pointers in the doubly-linked list of elements.
+	// To simplify the implementation, internally a list l is implemented
+	// as a ring, such that &l.root is both the next element of the last
+	// list element (l.Back()) and the previous element of the first list
+	// element (l.Front()).
+	next, prev *Element
+
+	// The list to which this element belongs.
+	list *List
+
+	// The value stored with this element.
+	Value any
 }
 
-// 添加节点
-func addNode(t *Node, v int) int {
-	if head == nil {
-		t = &Node{v, nil, nil}
-		head = t
-		return 0
+// Next returns the next list element or nil.
+func (e *Element) Next() *Element {
+	if p := e.next; e.list != nil && p != &e.list.root {
+		return p
 	}
-
-	if v == t.Value {
-		fmt.Println("节点已存在:", v)
-		return -1
-	}
-
-	// 如果当前节点下一个节点为空
-	if t.Next == nil {
-		// 与单链表不同的是每个节点还要维护前驱节点指针
-		temp := t
-		t.Next = &Node{v, temp, nil}
-		return -2
-	}
-
-	// 如果当前节点下一个节点不为空
-	return addNode(t.Next, v)
+	return nil
 }
 
-// 遍历链表
-func traverse(t *Node) {
-	if t == nil {
-		fmt.Println("-> 空链表!")
+// Prev returns the previous list element or nil.
+func (e *Element) Prev() *Element {
+	if p := e.prev; e.list != nil && p != &e.list.root {
+		return p
+	}
+	return nil
+}
+
+// List represents a doubly linked list.
+// The zero value for List is an empty list ready to use.
+type List struct {
+	root Element // sentinel list element, only &root, root.prev, and root.next are used
+	len  int     // current list length excluding (this) sentinel element
+}
+
+// Init initializes or clears list l.
+func (l *List) Init() *List {
+	l.root.next = &l.root
+	l.root.prev = &l.root
+	l.len = 0
+	return l
+}
+
+// New returns an initialized list.
+func New() *List { return new(List).Init() }
+
+// Len returns the number of elements of list l.
+// The complexity is O(1).
+func (l *List) Len() int { return l.len }
+
+// Front returns the first element of list l or nil if the list is empty.
+func (l *List) Front() *Element {
+	if l.len == 0 {
+		return nil
+	}
+	return l.root.next
+}
+
+// Back returns the last element of list l or nil if the list is empty.
+func (l *List) Back() *Element {
+	if l.len == 0 {
+		return nil
+	}
+	return l.root.prev
+}
+
+// lazyInit lazily initializes a zero List value.
+func (l *List) lazyInit() {
+	if l.root.next == nil {
+		l.Init()
+	}
+}
+
+// insert inserts e after at, increments l.len, and returns e.
+func (l *List) insert(e, at *Element) *Element {
+	e.prev = at
+	e.next = at.next
+	e.prev.next = e
+	e.next.prev = e
+	e.list = l
+	l.len++
+	return e
+}
+
+// insertValue is a convenience wrapper for insert(&Element{Value: v}, at).
+func (l *List) insertValue(v any, at *Element) *Element {
+	return l.insert(&Element{Value: v}, at)
+}
+
+// remove removes e from its list, decrements l.len
+func (l *List) remove(e *Element) {
+	e.prev.next = e.next
+	e.next.prev = e.prev
+	e.next = nil // avoid memory leaks
+	e.prev = nil // avoid memory leaks
+	e.list = nil
+	l.len--
+}
+
+// move moves e to next to at.
+func (l *List) move(e, at *Element) {
+	if e == at {
 		return
 	}
+	e.prev.next = e.next
+	e.next.prev = e.prev
 
-	for t != nil {
-		fmt.Printf("%d -> ", t.Value)
-		t = t.Next
-	}
-
-	fmt.Println()
+	e.prev = at
+	e.next = at.next
+	e.prev.next = e
+	e.next.prev = e
 }
 
-// 反向遍历链表
-func reverse(t *Node) {
-	if t == nil {
-		fmt.Println("-> 空链表!")
+// Remove removes e from l if e is an element of list l.
+// It returns the element value e.Value.
+// The element must not be nil.
+func (l *List) Remove(e *Element) any {
+	if e.list == l {
+		// if e.list == l, l must have been initialized when e was inserted
+		// in l or l == nil (e is a zero Element) and l.remove will crash
+		l.remove(e)
+	}
+	return e.Value
+}
+
+// PushFront inserts a new element e with value v at the front of list l and returns e.
+func (l *List) PushFront(v any) *Element {
+	l.lazyInit()
+	return l.insertValue(v, &l.root)
+}
+
+// PushBack inserts a new element e with value v at the back of list l and returns e.
+func (l *List) PushBack(v any) *Element {
+	l.lazyInit()
+	return l.insertValue(v, l.root.prev)
+}
+
+// InsertBefore inserts a new element e with value v immediately before mark and returns e.
+// If mark is not an element of l, the list is not modified.
+// The mark must not be nil.
+func (l *List) InsertBefore(v any, mark *Element) *Element {
+	if mark.list != l {
+		return nil
+	}
+	// see comment in List.Remove about initialization of l
+	return l.insertValue(v, mark.prev)
+}
+
+// InsertAfter inserts a new element e with value v immediately after mark and returns e.
+// If mark is not an element of l, the list is not modified.
+// The mark must not be nil.
+func (l *List) InsertAfter(v any, mark *Element) *Element {
+	if mark.list != l {
+		return nil
+	}
+	// see comment in List.Remove about initialization of l
+	return l.insertValue(v, mark)
+}
+
+// MoveToFront moves element e to the front of list l.
+// If e is not an element of l, the list is not modified.
+// The element must not be nil.
+func (l *List) MoveToFront(e *Element) {
+	if e.list != l || l.root.next == e {
 		return
 	}
-
-	temp := t
-	for t != nil {
-		temp = t
-		t = t.Next
-	}
-
-	for temp.Previous != nil {
-		fmt.Printf("%d -> ", temp.Value)
-		temp = temp.Previous
-	}
-
-	fmt.Printf("%d -> ", temp.Value)
-	fmt.Println()
+	// see comment in List.Remove about initialization of l
+	l.move(e, &l.root)
 }
 
-// 获取链表长度
-func size(t *Node) int {
-	if t == nil {
-		fmt.Println("-> 空链表!")
-		return 0
+// MoveToBack moves element e to the back of list l.
+// If e is not an element of l, the list is not modified.
+// The element must not be nil.
+func (l *List) MoveToBack(e *Element) {
+	if e.list != l || l.root.prev == e {
+		return
 	}
-
-	n := 0
-	for t != nil {
-		n++
-		t = t.Next
-	}
-
-	return n
+	// see comment in List.Remove about initialization of l
+	l.move(e, l.root.prev)
 }
 
-// 查找节点
-func lookupNode(t *Node, v int) bool {
-	if head == nil {
-		return false
+// MoveBefore moves element e to its new position before mark.
+// If e or mark is not an element of l, or e == mark, the list is not modified.
+// The element and mark must not be nil.
+func (l *List) MoveBefore(e, mark *Element) {
+	if e.list != l || e == mark || mark.list != l {
+		return
 	}
-
-	if v == t.Value {
-		return true
-	}
-
-	if t.Next == nil {
-		return false
-	}
-
-	return lookupNode(t.Next, v)
+	l.move(e, mark.prev)
 }
 
-// 初始化头节点
-var head = new(Node)
+// MoveAfter moves element e to its new position after mark.
+// If e or mark is not an element of l, or e == mark, the list is not modified.
+// The element and mark must not be nil.
+func (l *List) MoveAfter(e, mark *Element) {
+	if e.list != l || e == mark || mark.list != l {
+		return
+	}
+	l.move(e, mark)
+}
 
-func main() {
-	fmt.Println(head)
-	head = nil
-	// 遍历链表
-	traverse(head)
-	// 新增节点
-	addNode(head, 1)
-	// 再次遍历
-	traverse(head)
-	// 继续添加节点
-	addNode(head, 10)
-	addNode(head, 5)
-	addNode(head, 100)
-	// 再次遍历
-	traverse(head)
-	// 添加已存在节点
-	addNode(head, 100)
-	fmt.Println("链表长度:", size(head))
-	// 再次遍历
-	traverse(head)
-	// 反向遍历
-	reverse(head)
+// PushBackList inserts a copy of another list at the back of list l.
+// The lists l and other may be the same. They must not be nil.
+func (l *List) PushBackList(other *List) {
+	l.lazyInit()
+	for i, e := other.Len(), other.Front(); i > 0; i, e = i-1, e.Next() {
+		l.insertValue(e.Value, l.root.prev)
+	}
+}
 
-	// 查找已存在节点
-	if lookupNode(head, 5) {
-		fmt.Println("该节点已存在!")
-	} else {
-		fmt.Println("该节点不存在!")
+// PushFrontList inserts a copy of another list at the front of list l.
+// The lists l and other may be the same. They must not be nil.
+func (l *List) PushFrontList(other *List) {
+	l.lazyInit()
+	for i, e := other.Len(), other.Back(); i > 0; i, e = i-1, e.Prev() {
+		l.insertValue(e.Value, &l.root)
 	}
 }
---
+++ golang/go-Interview/data-structure/linear_list/link_list/double_linked_list_test.go
@@ -0,0 +1,65 @@
+/**
+    @author:HuChao
+    @data:2023/1/10
+    @note: doubly linked list test
+**/
+package linkedlist
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDoubleLinkedList(t *testing.T) {
+	// Create a new list and put some numbers in it.
+	l := New()            // New创建一个链表
+	e4 := l.PushBack(4)   // PushBack将一个值为v的新元素插入链表的最后一个位置，返回生成的新元素
+	e1 := l.PushFront(1)  // PushFront将一个值为v的新元素插入链表的第一个位置，返回生成的新元素。
+	l.InsertBefore(3, e4) // InsertBefore将一个值为v的新元素插入到mark前面，并返回生成的新元素。如果mark不是l的元素，l不会被修改。
+	l.InsertAfter(2, e1)  // InsertAfter将一个值为v的新元素插入到mark后面，并返回新生成的元素。如果mark不是l的元素，l不会被修改。
+	// Iterate through list and print its contents.
+	for e := l.Front(); e != nil; e = e.Next() { // 遍历得：1 2 3 4
+		fmt.Println(e.Value)
+	}
+
+	len := l.Len()                // Len返回链表中元素的个数，复杂度O(1)。
+	fmt.Println("链表中元素的个数:", len) // 4
+
+	front := l.Front()                   // Front返回链表第一个元素或nil。
+	fmt.Println("链表第一个元素:", front.Value) // 1
+
+	back := l.Back()                     // Back返回链表最后一个元素或nil。
+	fmt.Println("链表最后一个元素:", back.Value) // 4
+
+	l.PushFrontList(l) // PushFrontList创建链表other的拷贝，并将拷贝的最后一个位置连接到链表l的第一个位置。
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println("PushFrontList:", e.Value) // 1 2 3 4  1 2 3 4
+	}
+
+	l.PushBackList(l) // PushBackList创建链表other的拷贝，并将链表l的最后一个位置连接到拷贝的第一个位置。
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println("PushBackList:", e.Value) // 1 2 3 4  1 2 3 4  1 2 3 4 1 2 3 4
+	}
+
+	l.MoveToFront(e4) // MoveToFront将元素e移动到链表的第一个位置，如果e不是l的元素，l不会被修改。
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println("MoveToFront:", e.Value) // 4  1 2 3 4  1 2 3  1 2 3 4 1 2 3 4
+	}
+
+	l.MoveBefore(e1, e4) // MoveBefore将元素e移动到mark的前面。如果e或mark不是l的元素，或者e==mark，l不会被修改。
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println("MoveBefore:", e.Value) // 1 4  1 2 3 4  2 3  1 2 3 4 1 2 3 4
+	}
+
+	l.Remove(e1)
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println("Remove:", e.Value) // 4  1 2 3 4  2 3  1 2 3 4 1 2 3 4
+	}
+
+	l.Init() // Init清空链表。
+	// Iterate through list and print its contents.
+	for e := l.Front(); e != nil; e = e.Next() { // 遍历得：空
+		fmt.Println("Init清空链表", e.Value)
+	}
+
+}
