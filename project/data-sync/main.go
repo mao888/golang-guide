3533f76cfd96469d2d7de6442de34f899137ca5b
huchao <2557523039@qq.com>
2023-01-10 12:59:40 +0800

    feat:add VersionNumberToOrdinal

--- project/data-sync/main.go
+++ project/data-sync/main.go
@@ -1,15 +1,20 @@
 package main
 
-import mysql_to_mysql "github.com/mao888/golang-guide/project/data-sync/mysql-to-mysql"
+import (
+	mongo_to_mysql "github.com/mao888/golang-guide/project/data-sync/mongo-to-mysql"
+)
 
 func main() {
-	//data-sync.RunGame()
+	//mongo_to_mysql.RunGame()
 
 	// 将mongo数据迁移到mysql中的sdk_project表
-	//data-sync.RunSdkProject()
+	//mongo_to_mysql.RunSdkProject()
 
 	// 将mongo数据迁移到mysql中的sdk_release_record表
-	//data-sync.RunSdkReleaseRecord()
+	// mongo_to_mysql.RunSdkReleaseRecord()
+
+	// 更新 sdk_release_record表中 version_number 字段为 可排序字段 version_ordinal
+	mongo_to_mysql.VersionNumberToOrdinal()
 
 	// 将mongo数据迁移到mysql中的child_sdk表
 	//data-sync.RunChildSdk()
@@ -24,5 +29,5 @@ func main() {
 	//mysql_to_mysql.RunCommand()
 
 	// 将 gm-system/m_command_project_team gm-system/m_project_team_player 入库 application_console/gm_config
-	mysql_to_mysql.RunGmConfig()
+	// mysql_to_mysql.RunGmConfig()
 }
