f1766cf59ed845de62d2dc5943ff7ca97dc98cfb
huchao <2557523039@qq.com>
2023-02-23 11:46:15 +0800

    feat:Add

--- project/data-sync/mongo-to-mysql/ad_conf_centre/ad_conf_copywriting.go
+++ project/data-sync/mongo-to-mysql/ad_conf_centre/ad_conf_copywriting.go
@@ -3,6 +3,7 @@ package ad_conf_centre
 import (
 	"context"
 	"fmt"
+	"github.com/mao888/go-utils/constants"
 	db2 "github.com/mao888/golang-guide/project/data-sync/db"
 	"github.com/mao888/golang-guide/project/data-sync/mongo-to-mysql/ad_conf_centre/bean"
 	"go.mongodb.org/mongo-driver/bson"
@@ -30,7 +31,7 @@ func RunAdConfCopywriting() {
 		fmt.Println("Mongo查询MGame错误：", err)
 	}
 	// 3、将从mongo中查出的games.id(int)作为key, games.game_id(string)作为value,存入map
-	idMap := map[interface{}]string{}
+	idMap := map[int32]string{}
 	for _, game := range mGame {
 		idMap[game.ID] = game.GameID
 	}
@@ -38,101 +39,132 @@ func RunAdConfCopywriting() {
 	// 3、将mongo数据装入切片
 	//adConfAudience := make([]*bean.AdConfAudience, 0)
 	for _, text := range mAdTexts {
-		var adConfCopywriting *bean.AdConfCopywriting
-		adConfCopywriting.GameID = idMap[text.GameId]
+		var adConfCopywriting bean.AdConfCopywriting
+
+		// GameID
+		var gameID string
+		if text.GameId == constants.EmptyString {
+			gameID = ""
+		} else {
+			p, ok := text.GameId.(int32)
+			if ok {
+				gameID = idMap[p]
+			}
+		}
+		adConfCopywriting.GameID = gameID
 		adConfCopywriting.DefaultLanguage = bean.LanguagesMapCodeToShortName[text.DefaultLang]
 		adConfCopywriting.CreatedAt = text.CreateTime.Unix()
 		adConfCopywriting.UpdatedAt = text.UpdateTime.Unix()
 		adConfCopywriting.DefaultLanguageContent = text.DefaultText
+		var (
+			en   string
+			de   string
+			fr   string
+			ja   string
+			ko   string
+			es   string
+			it   string
+			zhtw string
+			ar   string
+			th   string
+			ru   string
+			pt   string
+			nl   string
+			tr   string
+			vi   string
+			ms   string
+			id   string
+			tl   string
+		)
 		for _, s := range text.Translation {
 			lang := bean.LanguagesMapCodeToShortName[s.Lang]
 			//En
-			var en string
+			//var en string
 			if lang == "en" {
 				en = s.Text
 			}
 			//De
-			var de string
+			//var de string
 			if lang == "de" {
 				de = s.Text
 			}
 			//Fr
-			var fr string
+			//var fr string
 			if lang == "fr" {
 				fr = s.Text
 			}
 			//Ja
-			var ja string
+			//var ja string
 			if lang == "ja" {
 				ja = s.Text
 			}
 			//Ko
-			var ko string
+			//var ko string
 			if lang == "ko" {
 				ko = s.Text
 			}
 			//Es
-			var es string
+			//var es string
 			if lang == "es" {
 				es = s.Text
 			}
 			//It
-			var it string
+			//var it string
 			if lang == "it" {
 				it = s.Text
 			}
 			//ZhTw
-			var zhtw string
+			//var zhtw string
 			if lang == "zh-tw" {
 				zhtw = s.Text
 			}
 			//Ar
-			var ar string
+			//var ar string
 			if lang == "ar" {
 				ar = s.Text
 			}
 			//Th
-			var th string
+			//var th string
 			if lang == "th" {
 				th = s.Text
 			}
 			//Ru
-			var ru string
+			//var ru string
 			if lang == "ru" {
 				ru = s.Text
 			}
 			//Pt
-			var pt string
+			//var pt string
 			if lang == "pt" {
 				pt = s.Text
 			}
 			//Nl
-			var nl string
+			//var nl string
 			if lang == "nl" {
 				nl = s.Text
 			}
 			//Tr
-			var tr string
+			//var tr string
 			if lang == "tr" {
 				tr = s.Text
 			}
 			//Vi
-			var vi string
+			//var vi string
 			if lang == "vi" {
 				vi = s.Text
 			}
 			//Ms
-			var ms string
+			//var ms string
 			if lang == "ms" {
 				ms = s.Text
 			}
 			//Iid
-			var id string
+			//var id string
 			if lang == "id" {
 				id = s.Text
 			}
 			//Tl
-			var tl string
+			//var tl string
 			if lang == "tl" {
 				tl = s.Text
 			}
@@ -156,7 +188,7 @@ func RunAdConfCopywriting() {
 			adConfCopywriting.Tl = tl
 		}
 		// 4、将装有mongo数据的切片入库
-		err = db2.MySQLClientCruiser.Table("ad_conf_copywriting").Create(adConfCopywriting).Error
+		err = db2.MySQLClientCruiser.Table("ad_conf_copywriting").Create(&adConfCopywriting).Error
 		if err != nil {
 			fmt.Println("入mysql/ad_conf_copywriting 错误：", err)
 		}
