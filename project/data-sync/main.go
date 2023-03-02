3cafe4a2e7f16af8a0d4d50956ebd3483d0c9414
huchao <2557523039@qq.com>
2023-03-02 15:40:22 +0800

    fix:版本检查数据迁移 更新 is_gray

--- project/data-sync/main.go
+++ project/data-sync/main.go
@@ -1,7 +1,5 @@
 package main
 
-import version_console "github.com/mao888/golang-guide/project/data-sync/mongo-to-mysql/version-console"
-
 func main() {
 	// mongo_to_mysql.RunGame()
 
@@ -47,7 +45,7 @@ func main() {
 
 	// 四、版本检查数据迁移
 	// Mongo/plat_console/environments 数据迁移到 ARK version_console/env
-	version_console.RunEnvAndVersion()
+	//version_console.RunEnvAndVersion()
 
 	//Mongo/plat_console/gamelanguageconfs 数据迁移到 ARK version_console/language
 	//version_console.RunLanguage()
