e01f121eda031eb3712cdede84aba443f36e0fe7
huchao <2557523039@qq.com>
2023-08-03 20:38:23 +0800

    feat:add signature

--- golang/go-study/exercise/Go/Time/时间戳转换成UTC时间.go
+++ golang/go-study/exercise/Go/Time/时间戳转换成UTC时间.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"fmt"
-	"github.com/mao888/go-utils/constants"
 	"time"
 )
 
 func main() {
-	chuo := 1668583367
-	bendi := time.Unix(int64(chuo), 0).Format(constants.TimeYMDHMM)
-	lastLoginAt, err := time.Parse(constants.TimeYMDHMM, bendi)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(lastLoginAt.String())
+	chuo := int64(1688589582)
+	//bendi := time.Unix(int64(chuo), 0).Format("2006-01-02 15:04:05")
+	//lastLoginAt, err := time.Parse("2006-01-02 15:04:05", bendi)
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//fmt.Println(lastLoginAt.String())
+
+	// 将时间戳转换为北京时间
+	beijng := time.Unix(chuo, 0).In(time.FixedZone("CST", 8*3600)).Format("2006-01-02 15:04:05")
+
+	// 将时间戳转换为UTC时间
+	utc := time.Unix(chuo, 0).UTC().Format("2006-01-02 15:04:05")
+
+	fmt.Println("beijng", beijng)
+	fmt.Println("utc", utc)
 }
---
+++ golang/go-study/exercise/Go/test/signature.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"fmt"
+	"sort"
+)
+
+func main() {
+	s1 := Signature("1760312311655437", "1688719212", "1236080353")
+	s2 := Signature("1760312311655437", "1690803473", "1236080353")
+	s3 := Signature("52F118BF3C8596B44E31BFD45D4B15B7", "1688719212", "1236080353")
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+}
+
+// Signature 对密钥token，timestamp和nonce三个string进行签名 生成 signature
+//1. 对密钥token，timestamp和nonce三个string进行升序排序
+//2. 将三个string转成bytes放入一个buffer
+//3. 对byte buffer进行sha1 哈希加密
+//4. 哈希加密后转成string即为signature
+//5. 然后将string拼在url上
+func Signature(token string, timestamp string, nonce string) string {
+	strList := []string{token, timestamp, nonce}
+	sort.Strings(strList)
+	var buffer bytes.Buffer
+	for _, str := range strList {
+		buffer.WriteString(str)
+	}
+	h := sha1.New()
+	h.Write(buffer.Bytes())
+	r := fmt.Sprintf("%x", h.Sum(nil))
+	return r
+}
