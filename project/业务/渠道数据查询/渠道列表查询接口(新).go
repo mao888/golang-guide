4fe2901ebd5e6f723d1d6646ce295b6cbb4a7e7c
huchao <2557523039@qq.com>
2023-11-02 17:46:51 +0800

    feat:渠道列表查询接口(新) && 用户数据查询

---
+++ project/业务/渠道数据查询/渠道列表查询接口(新).go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"gopkg.in/resty.v1"
+	"strings"
+	"time"
+)
+
+type ChannelInfoList struct {
+	RetCode int    `json:"retCode"`
+	RetMsg  string `json:"retMsg"`
+	Data    struct {
+		ChannelList     []string `json:"channelList"`
+		ChannelInfoList []struct {
+			ChannelId string `json:"channelId"`
+			UserName  string `json:"userName"`
+			NickName  string `json:"nickName"`
+		} `json:"channelInfoList"`
+	} `json:"data"`
+}
+
+func main() {
+
+	clientId := "10007348"
+	//当前时间戳(毫秒)
+	timestamp := time.Now().UnixNano() / 1e6
+	token := "AGy97zKlbzEArFwNDh"
+	//签名秘钥(小写md5(clientId+接口token+timestamp))
+	signKey := strings.ToLower(fmt.Sprintf("%x", md5.Sum([]byte(clientId+token+fmt.Sprintf("%v", timestamp)))))
+
+	url := "https://routine.wqxsw.com/flames/channel/query/channelInfoList"
+	resp, err := resty.New().SetRetryCount(3).R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(map[string]interface{}{
+			"clientId":  clientId,  //clientId
+			"timestamp": timestamp, //时间戳(毫秒)
+			"signKey":   signKey,   //签名秘钥(小写md5(clientId+接口token+timestamp))
+		}).
+		Post(url)
+	if err != nil {
+		fmt.Println("Post err", err)
+		return
+	}
+
+	var channelInfoList ChannelInfoList
+	err = json.Unmarshal(resp.Body(), &channelInfoList)
+	if err != nil {
+		fmt.Println("Unmarshal err:", err)
+		return
+	}
+	fmt.Printf("channelInfoList:%+v\n", channelInfoList)
+	// channelInfoList:{RetCode:0 RetMsg:成功 Data:{ChannelList:[29576 29577 29578 29579 29580] ChannelInfoList:[{ChannelId:29576 UserName:beitadj1 NickName:无限短剧-头条直投1-贝塔} {ChannelId:29577 UserName:beitadj2 NickName:无限短剧-头lId:29578 UserName:beitadj3 NickName:无限短剧-头条直投3-贝塔} {ChannelId:29579 UserName:beitadj4 NickName:无限短剧-头条直投4-贝塔} {ChannelId:29580 UserName:beitadj5 NickName:无限短剧-头条直投5-贝塔}]}}
+}
---
+++ project/业务/渠道数据查询/用户数据查询.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"gopkg.in/resty.v1"
+	"strings"
+	"time"
+)
+
+type User struct {
+	RetCode int    `json:"retCode"`
+	RetMsg  string `json:"retMsg"`
+	Data    string `json:"data"`
+}
+
+func main() {
+
+	clientId := "10007348"
+	// 当前时间戳(毫秒)
+	timestamp := time.Now().UnixNano() / 1e6
+	token := "AGy97zKlbzEArFwNDh"
+	//签名秘钥(小写md5(clientId+接口token+timestamp))
+	signKey := strings.ToLower(fmt.Sprintf("%x", md5.Sum([]byte(clientId+token+fmt.Sprintf("%v", timestamp)))))
+
+	url := "https://routine.wqxsw.com/flames/channel/query/user"
+	resp, err := resty.New().SetRetryCount(3).R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(map[string]interface{}{
+			"clientId":   clientId,     //clientId
+			"sdate":      "1698768001", //开始时间-时间戳(秒)
+			"queryField": "loginTime",  //支持自定义字段查询：此字段不为空使用loginTime，否则使用ctime.loginTime[用户活跃时间]-满足每日新增、每日活跃数据统计;ctime[用户注册时间]
+			"edate":      "1698854399", //结束时间-时间戳(秒)
+			"channelCodeList": []string{
+				"29576",
+				"29577",
+				"29578",
+				"29579",
+				"29580",
+			},
+			"timestamp": timestamp, //时间戳(毫秒)
+			"signKey":   signKey,   //签名秘钥(小写md5(clientId+接口token+timestamp))
+		}).
+		Post(url)
+	if err != nil {
+		fmt.Println("Post err", err)
+		return
+	}
+
+	var user User
+	err = json.Unmarshal(resp.Body(), &user)
+	if err != nil {
+		fmt.Println("Unmarshal err:", err)
+		return
+	}
+	fmt.Printf("user:%+v\n", user) // user:{RetCode:0 RetMsg:成功 Data:OnNLbXpKRQLcqgN}
+}
