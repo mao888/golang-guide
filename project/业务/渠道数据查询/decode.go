8c80e540e0baa9e2097c5fab519fd03012892bf2
huchao <2557523039@qq.com>
2023-11-02 19:33:37 +0800

    feat:Ddecode

--- project/业务/渠道数据查询/decode.go
+++ project/业务/渠道数据查询/decode.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/resty.v1"
 	"net/url"
 )
 
 func main() {
 	// URL字符串
-	urlStr := "https%3A%2F%2Fxcxoss.dzyds.com%2Fdownload%2Fchannel%2F20231102%2F10007348_alias_cpsvideo_user_1698918329171.txt%3FtaskId%3DOnNLbXpKRQLcqgN&dataType=alias_user"
+	// user
+	//urlStr := "https%3A%2F%2Fxcxoss.dzyds.com%2Fdownload%2Fchannel%2F20231102%2F10007348_alias_cpsvideo_user_1698918329171.txt%3FtaskId%3DOnNLbXpKRQLcqgN&dataType=alias_user"
+	// order
+	urlStr := "https%3A%2F%2Fxcxoss.dzyds.com%2Fdownload%2Fchannel%2F20231102%2F10007348_alias_cpsvideo_order_1698920405212.txt%3FtaskId%3DO4jM0nylczk118F&dataType=alias_order"
 
 	// 对URL进行解码
 	decodedURL, err := url.QueryUnescape(urlStr)
@@ -18,5 +22,15 @@ func main() {
 
 	// 打印解码后的URL
 	fmt.Println("解码后的URL:", decodedURL)
-	// 用户数据查询 解码后的URL: https://xcxoss.dzyds.com/download/channel/20231102/10007348_alias_cpsvideo_user_1698918329171.txt?taskId=OnNLbXpKRQLcqgN&dataType=alias_user
+	// user 解码后的URL: https://xcxoss.dzyds.com/download/channel/20231102/10007348_alias_cpsvideo_user_1698918329171.txt?taskId=OnNLbXpKRQLcqgN&dataType=alias_user
+	// order 解码后的URL: https://xcxoss.dzyds.com/download/channel/20231102/10007348_alias_cpsvideo_order_1698920405212.txt?taskId=O4jM0nylczk118F&dataType=alias_order
+
+	resp, err := resty.New().SetRetryCount(3).R().
+		SetHeader("Content-Type", "application/json").
+		Get(decodedURL)
+	if err != nil {
+		fmt.Println("Get err", err)
+		return
+	}
+	fmt.Println("resp:", resp)
 }
