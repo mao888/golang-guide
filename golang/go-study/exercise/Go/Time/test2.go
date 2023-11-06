fc730b9cff79f180ac1b884544a6b0d9d92ac29d
huchao <2557523039@qq.com>
2023-11-06 18:33:40 +0800

    feat:time/test2

---
+++ golang/go-study/exercise/Go/Time/test2.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mao888/go-utils/constants"
+	"time"
+)
+
+func main() {
+	isHour := false
+	//data := "2023-11-06 18"
+	data := "2023-11-06"
+	loa := time.FixedZone("Asia/Shanghai", 8*60*60)
+	var sdate, edate int64
+	if isHour {
+		date, err := time.ParseInLocation(constants.TimeYMDH, data, loa)
+		if err != nil {
+			fmt.Println("err:", err)
+			return
+		}
+		sdate = date.Unix()
+		edate = date.Add(time.Minute * 60).Unix() // sdate: 1699264800 edate: 1699268400
+	} else {
+		date, err := time.ParseInLocation(constants.TimeYMD, data, loa)
+		if err != nil {
+			fmt.Println("err:", err)
+			return
+		}
+		sdate = date.Unix()
+		edate = date.Add(time.Hour * 24).Unix() // sdate: 1699200000 edate: 1699286400
+	}
+	fmt.Println("sdate:", sdate, "edate:", edate)
+}
