ac20fe60e9465fb973c24747d7136bdce9787326
huchao <2557523039@qq.com>
2023-11-16 11:09:55 +0800

    feat:defer

---
+++ golang/go-study/exercise/Go/defer/test1.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func f1() int {
+	x := 5
+	defer func() {
+		x++
+	}()
+	return x
+}
+
+func f2() (x int) {
+	defer func() {
+		x++
+	}()
+	return 5
+}
+
+func f3() (y int) {
+	x := 5
+	defer func() {
+		x++
+	}()
+	return x
+}
+func f4() (x int) {
+	defer func(x int) {
+		x++
+	}(x)
+	return 5
+}
+func main() {
+	fmt.Println(f1()) // 5
+	fmt.Println(f2()) // 6
+	fmt.Println(f3()) // 5
+	fmt.Println(f4()) // 5
+}
