c80aae7663f26849fc4f37fc825506060e93d323
huchao <2557523039@qq.com>
2023-11-21 16:47:27 +0800

    feat:小游戏归因/巨量/微信/signature

--- project/业务/三方抓数/Taurusx/广告投放报表.go
+++ project/业务/三方抓数/Taurusx/广告投放报表.go
@@ -30,55 +30,63 @@ type (
 	}
 )
 
+var (
+	timestamp = time.Now().Unix()
+)
+
 func main() {
 	var (
-		timestamp = time.Now().Unix()
-		token     string
-		url       string
+		//timestamp = time.Now().Unix()
+		token string
+		url   string
 
 		baseUrl   = "https://scaler.taurusx.com/openapi/performance_data"
 		accessKey = "a7d8dece052147f598c6b57fb2fa5bcb"
 		apiKey    = "e1a476536eac4e60b727b570c7140be5"
 
-		timezone        = "+0"
-		startTime       = "2023-11-01"
-		endTime         = "2023-11-05"
+		timezone = "+0"
+		//startTime       = "2023-11-21"
+		//endTime         = "2023-11-30"
 		dimensionOption = "Campaign,Adgroup,Creative,AdType,Country"
+		datas           = []string{"2023-11-21", "2023-11-22", "2023-11-23", "2023-11-24", "2023-11-25", "2023-11-26", "2023-11-27", "2023-11-28", "2023-11-29", "2023-11-30"}
 	)
 	token = generateHash2(apiKey)
 
-	url = fmt.Sprintf("%s?timezone=%s&start_time=%s&end_time=%s&dimension_option=%s",
-		baseUrl, timezone, startTime, endTime, dimensionOption)
-	fmt.Println("url:", url)
+	for i, data := range datas {
+		fmt.Println("i:", i, "data:", data)
 
-	resp, err := resty.New().SetRetryCount(3).R().
-		SetHeaders(map[string]string{
-			"Content-Type": "application/json",
-			"access-key":   accessKey,
-			"token":        token,
-			"timestamp":    fmt.Sprintf("%d", timestamp),
-		}).
-		Get(url)
-	fmt.Println("resp:", string(resp.Body()))
-	if err != nil {
-		fmt.Println("Get err", err)
-		return
+		url = fmt.Sprintf("%s?timezone=%s&start_time=%s&end_time=%s&dimension_option=%s",
+			baseUrl, timezone, data, data, dimensionOption)
+		fmt.Println("url:", url)
+
+		resp, err := resty.New().SetRetryCount(3).R().
+			SetHeaders(map[string]string{
+				"Content-Type": "application/json",
+				"access-key":   accessKey,
+				"token":        token,
+				"timestamp":    fmt.Sprintf("%d", timestamp),
+			}).
+			Get(url)
+		fmt.Println("resp:", string(resp.Body()))
+		if err != nil {
+			fmt.Println("Get err", err)
+			return
+		}
+
+		var taurusXRes TaurusXRes
+		err = json.Unmarshal(resp.Body(), &taurusXRes)
+		if err != nil {
+			fmt.Println("Unmarshal err", err)
+			return
+		}
+		fmt.Printf("len: %d, taurusXRes:%+v\n", len(taurusXRes.Data.DataList),
+			taurusXRes)
 	}
-
-	var taurusXRes TaurusXRes
-	err = json.Unmarshal(resp.Body(), &taurusXRes)
-	if err != nil {
-		fmt.Println("Unmarshal err", err)
-		return
-	}
-	fmt.Printf("len: %d, taurusXRes:%+v\n", len(taurusXRes.Data.DataList),
-		taurusXRes)
-
 }
 
 func generateHash2(apiKey string) string {
 	// 获取当前时间戳
-	timestamp := time.Now().Unix()
+	//timestamp := time.Now().Unix()
 
 	// 对时间戳进行MD5哈希
 	timestampHashInBytes := md5.Sum([]byte(fmt.Sprintf("%d", timestamp)))
---
+++ project/业务/小游戏归因/巨量/微信/signature.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/json"
+	"fmt"
+	"github.com/golang/glog"
+	"gopkg.in/resty.v1"
+	"math/rand"
+	"sort"
+	"strconv"
+	"time"
+)
+
+type (
+	WeChatForwardResp struct {
+		Data    string `json:"data"`
+		Message string `json:"message"`
+		Status  int    `json:"status"`
+	}
+)
+
+const WeChatMiniGameForwardUrl = "https://clue.oceanengine.com/outer/wechat/applet/token/"
+
+func main() {
+	var (
+		token     = "52F118BF3C8596B44E31BFD45D4B15B7"
+		timestamp = time.Now().Unix()
+		urlToken  = "1760312311655437"
+
+		clickId = "1779451543470157,1783156862460969"
+		openId  = "oso8q5BZxVv5QqKiJHQirpFCZ7GA"
+	)
+
+	rand.Seed(time.Now().UnixNano())
+	nonce := strconv.Itoa(rand.Intn(100000000))
+
+	s := signature(token, fmt.Sprintf("%d", timestamp), nonce)
+
+	url := fmt.Sprintf("%s%s?timestamp=%d&nonce=%s&signature=%s",
+		WeChatMiniGameForwardUrl, urlToken, timestamp, nonce, s)
+	glog.Infof("weChatMiniActiveEvent url: %s", url)
+
+	resp, err := resty.New().SetRetryCount(3).R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(map[string]interface{}{
+			"clue_token": clickId, // 用户跳转小程序时携带的clue_token参数
+			"open_id":    openId,  // 微信open_id，必须回传
+			"event_type": "0",     // 事件类型，string格式
+		}).
+		Post(url)
+	if err != nil {
+		glog.Errorf("weChatMiniActiveEvent resty.New().R().Post err: %v", err)
+		return
+	}
+	fmt.Println("resp:", string(resp.Body()))
+
+	var weChatResp WeChatForwardResp
+	err = json.Unmarshal(resp.Body(), &weChatResp)
+	if err != nil {
+		glog.Errorf("weChatMiniActiveEvent json.Unmarshal err: %v", err)
+	}
+	glog.Infof("weChatMiniActiveEvent weChatResp: %+v", weChatResp)
+}
+
+func signature(token string, timestamp string, nonce string) string {
+	strList := []string{token, timestamp, nonce}
+	sort.Strings(strList)
+	var buffer bytes.Buffer
+	for _, str := range strList {
+		buffer.WriteString(str)
+	}
+	h := sha1.New()
+	h.Write(buffer.Bytes())
+	r := fmt.Sprintf("%x", h.Sum(nil))
+	return r
+}
