94257ce19692d65df1c4099106b28375d43e36ed
huchao <2557523039@qq.com>
2023-11-22 11:50:08 +0800

    fix:/Taurusx/广告投放报表

--- project/业务/三方抓数/Taurusx/广告投放报表.go
+++ project/业务/三方抓数/Taurusx/广告投放报表.go
@@ -30,65 +30,45 @@ type (
 	}
 )
 
-//var (
-//	//timestamp = time.Now().Unix()
-//)
+var (
+	//timestamp = time.Now().Unix()
+	token string
+	url   string
+
+	baseUrl   = "https://scaler.taurusx.com/openapi/performance_data"
+	accessKey = "a7d8dece052147f598c6b57fb2fa5bcb"
+	apiKey    = "e1a476536eac4e60b727b570c7140be5"
+
+	timezone = "+0"
+	//startTime       = "2023-11-21"
+	//endTime         = "2023-11-30"
+	dimensionOption = "Campaign,Adgroup,Creative,AdType,Country"
+	datas           = []string{"2023-11-20", "2023-11-21", "2023-11-22"}
+	//datas = []string{"2023-11-19"}
+	//timestamp = time.Now().Unix()
+)
 
 func main() {
-	var (
-		//timestamp = time.Now().Unix()
-		token string
-		url   string
-
-		baseUrl   = "https://scaler.taurusx.com/openapi/performance_data"
-		accessKey = "a7d8dece052147f598c6b57fb2fa5bcb"
-		apiKey    = "e1a476536eac4e60b727b570c7140be5"
-
-		timezone = "+0"
-		//startTime       = "2023-11-21"
-		//endTime         = "2023-11-30"
-		dimensionOption = "Campaign,Adgroup,Creative,AdType,Country"
-		datas           = []string{"2023-11-20", "2023-11-21", "2023-11-22", "2023-11-23", "2023-11-24", "2023-11-25", "2023-11-26", "2023-11-27", "2023-11-28", "2023-11-29", "2023-11-30"}
-
-		timestamp = time.Now().Unix()
-	)
 	token = generateHash2(apiKey)
-
+	//time.Sleep(1 * time.Second)
+	timestamp := time.Now().Unix()
+	println("getPerformanceData时间戳", timestamp)
 	for i, data := range datas {
 		fmt.Println("i:", i, "data:", data)
-
-		url = fmt.Sprintf("%s?timezone=%s&start_time=%s&end_time=%s&dimension_option=%s",
-			baseUrl, timezone, data, data, dimensionOption)
-		fmt.Println("url:", url)
-
-		resp, err := resty.New().SetRetryCount(3).R().
-			SetHeaders(map[string]string{
-				"Content-Type": "application/json",
-				"access-key":   accessKey,
-				"token":        token,
-				"timestamp":    fmt.Sprintf("%d", timestamp),
-			}).
-			Get(url)
-		fmt.Println("resp:", string(resp.Body()))
+		res, err := getPerformanceData(token, data, timestamp)
 		if err != nil {
-			fmt.Println("Get err", err)
+			fmt.Println("getPerformanceData err", err)
 			return
 		}
-
-		var taurusXRes TaurusXRes
-		err = json.Unmarshal(resp.Body(), &taurusXRes)
-		if err != nil {
-			fmt.Println("Unmarshal err", err)
-			return
-		}
-		fmt.Printf("len: %d, taurusXRes:%+v\n", len(taurusXRes.Data.DataList),
-			taurusXRes)
+		fmt.Printf("len: %d, taurusXRes:%+v\n", len(res),
+			res)
 	}
 }
 
 func generateHash2(apiKey string) string {
 	// 获取当前时间戳
 	timestamp := time.Now().Unix()
+	println("token时间戳", timestamp)
 
 	// 对时间戳进行MD5哈希
 	timestampHashInBytes := md5.Sum([]byte(fmt.Sprintf("%d", timestamp)))
@@ -100,4 +80,34 @@ func generateHash2(apiKey string) string {
 	hash := hex.EncodeToString(hashInBytes[:])
 
 	return hash
+}
+
+func getPerformanceData(token, date string, timestamp int64) ([]TaurusX, error) {
+	url := fmt.Sprintf("%s?timezone=%s&start_time=%s&end_time=%s&dimension_option=%s",
+		baseUrl, timezone, date, date, dimensionOption)
+	fmt.Println("url:", url)
+
+	resp, err := resty.New().SetRetryCount(3).R().
+		SetHeaders(map[string]string{
+			"Content-Type": "application/json",
+			"access-key":   accessKey,
+			"token":        token,
+			"timestamp":    fmt.Sprintf("%d", timestamp),
+		}).
+		Get(url)
+	if err != nil {
+		fmt.Println("Get err", err)
+		return nil, err
+	}
+	fmt.Println("resp:", string(resp.Body()))
+
+	var taurusXRes TaurusXRes
+	err = json.Unmarshal(resp.Body(), &taurusXRes)
+	if err != nil {
+		fmt.Println("Unmarshal err", err)
+		return nil, err
+	}
+	//fmt.Printf("len: %d, taurusXRes:%+v\n", len(taurusXRes.Data.DataList),
+	//	taurusXRes)
+	return taurusXRes.Data.DataList, nil
 }
