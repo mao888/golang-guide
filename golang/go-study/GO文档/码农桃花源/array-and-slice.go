e3c279e935a6493379f0a60262f576467e534f0c
huchao <2557523039@qq.com>
2024-01-02 18:59:44 +0800

    feat:golang/go-study/GO文档/码农桃花源/array-and-slice

---
+++ golang/go-study/GO文档/码农桃花源/array-and-slice.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	s1 := slice[2:5]
+	s2 := s1[2:6:7]
+
+	s2 = append(s2, 100)
+	s2 = append(s2, 200)
+
+	s1[2] = 20
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(slice)
+}
