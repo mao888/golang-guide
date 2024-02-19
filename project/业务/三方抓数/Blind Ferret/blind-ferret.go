2d5e04d052141c11a24176bb1dedceb34192a0ce
huchao <2557523039@qq.com>
2024-02-19 17:58:06 +0800

    feat:blind-ferret 抓数

--- project/业务/三方抓数/Blind Ferret/blind-ferret.go
+++ project/业务/三方抓数/Blind Ferret/blind-ferret.go
@@ -10,30 +10,30 @@ import (
 
 type (
 	BlindFerretRes struct {
-		Date                 string      `json:"Date"`
-		App                  string      `json:"App"`
-		StoreIDBundleID      string      `json:"Store ID (Bundle ID)"`
-		FlourishCampaignID   string      `json:"Flourish Campaign ID"`
-		FlourishCampaignName string      `json:"Flourish Campaign Name"`
-		CampaignID           string      `json:"Campaign ID"`
-		CampaignName         string      `json:"Campaign Name"`
-		SubCampaignID        string      `json:"Sub-Campaign ID"`
-		SubCampaignName      string      `json:"Sub-Campaign Name"`
-		FlourishOfferName    string      `json:"Flourish Offer Name"`
-		PublisherName        string      `json:"Publisher Name"`
-		PublisherID          string      `json:"Publisher ID"`
-		TrackingURL          string      `json:"Tracking URL"`
-		CreativeID           interface{} `json:"Creative ID"`
-		CreativeName         interface{} `json:"Creative Name"`
-		CreativeURL          interface{} `json:"Creative URL"`
-		Platform             string      `json:"Platform"`
-		Country              string      `json:"Country"`
-		ReportingTimezone    string      `json:"Reporting Timezone"`
-		Currency             string      `json:"Currency"`
-		Impressions          *string     `json:"Impressions"`
-		Clicks               *string     `json:"Clicks"`
-		Installs             *string     `json:"Installs"`
-		Cost                 *string     `json:"Cost"`
+		Date                 string `json:"Date"`
+		App                  string `json:"App"`
+		StoreIDBundleID      string `json:"Store ID (Bundle ID)"`
+		FlourishCampaignID   string `json:"Flourish Campaign ID"`
+		FlourishCampaignName string `json:"Flourish Campaign Name"`
+		CampaignID           string `json:"Campaign ID"`
+		CampaignName         string `json:"Campaign Name"`
+		SubCampaignID        string `json:"Sub-Campaign ID"`
+		SubCampaignName      string `json:"Sub-Campaign Name"`
+		FlourishOfferName    string `json:"Flourish Offer Name"`
+		PublisherName        string `json:"Publisher Name"`
+		PublisherID          string `json:"Publisher ID"`
+		TrackingURL          string `json:"Tracking URL"`
+		CreativeID           string `json:"Creative ID"`
+		CreativeName         string `json:"Creative Name"`
+		CreativeURL          string `json:"Creative URL"`
+		Platform             string `json:"Platform"`
+		Country              string `json:"Country"`
+		ReportingTimezone    string `json:"Reporting Timezone"`
+		Currency             string `json:"Currency"`
+		Impressions          string `json:"Impressions"`
+		Clicks               string `json:"Clicks"`
+		Installs             string `json:"Installs"`
+		Cost                 string `json:"Cost"`
 	}
 )
 
@@ -42,7 +42,7 @@ func main() {
 	var (
 		format = "json"
 		apiKey = "df0a831e4c69906642a435e452c40b93"
-		date   = "2024-01-01"
+		date   = "2024-02-01"
 	)
 	url := fmt.Sprintf("https://engage-network.influencemobile.com/reports/v2/offers.%s?api_key=%s&date=%s", format, apiKey, date)
 	resp, err := resty.New().SetRetryCount(3).R().Get(url)
@@ -50,7 +50,7 @@ func main() {
 		glog.Errorf(ctx, "Get err:%s", err)
 		return
 	}
-	glog.Infof(ctx, "resp:%s", string(resp.Body()))
+	//glog.Infof(ctx, "resp:%s", string(resp.Body()))
 
 	var blindFerretRes []BlindFerretRes
 	err = json.Unmarshal(resp.Body(), &blindFerretRes)
@@ -58,6 +58,6 @@ func main() {
 		glog.Errorf(ctx, "Unmarshal err:%s", err)
 		return
 	}
-	glog.Infof(ctx, "blindFerretRes:%v", blindFerretRes)
-	glog.Infof(ctx, "blindFerretRes的长度:%d", blindFerretRes)
+	//glog.Infof(ctx, "blindFerretRes:%v", blindFerretRes)
+	glog.Infof(ctx, "blindFerretRes的长度:%d", len(blindFerretRes))
 }
