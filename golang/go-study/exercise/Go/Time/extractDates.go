14cb3a05e8eb5c303f0c1426c73e1231183f073c
huchao <2557523039@qq.com>
2024-02-20 15:49:24 +0800

    feat:golang/go-study/exercise/Go/Time/extractDates

--- golang/go-study/exercise/Go/Time/extractDates.go
+++ golang/go-study/exercise/Go/Time/extractDates.go
@@ -2,40 +2,42 @@ package main
 
 import (
 	"fmt"
+	"github.com/mao888/mao-gutils/constants"
 	"time"
 )
 
-func extractDates(dates []string) []string {
+func ExtractDates(dates []string) ([]string, error) {
 	if len(dates) == 0 {
-		return nil
+		return []string{}, nil
 	}
 
 	// 解析第一个日期
-	firstDate, err := time.Parse("2006-01-02", dates[0])
+	firstDate, err := time.Parse(constants.TimeYMD, dates[0])
 	if err != nil {
-		fmt.Println("Error parsing date:", err)
-		return nil
+		return nil, err
 	}
 
 	// 解析最后一个日期
-	lastDate, err := time.Parse("2006-01-02", dates[len(dates)-1])
+	lastDate, err := time.Parse(constants.TimeYMD, dates[len(dates)-1])
 	if err != nil {
-		fmt.Println("Error parsing date:", err)
-		return nil
+		return nil, err
 	}
 
 	// 判断是否跨月
 	if firstDate.Month() == lastDate.Month() {
-		// 没有跨月，返回第一天和最后一天
-		return []string{lastDate.Format("2006-01-02")}
+		// 没有跨月，直接返回最后一天
+		return []string{dates[len(dates)-1]}, nil
 	} else {
-		// 跨月了，直接返回最后一天
-		return []string{firstDate.Format("2006-01-02"), lastDate.Format("2006-01-02")}
+		// 跨月了，返回第一天和最后一天
+		return []string{dates[0], dates[len(dates)-1]}, nil
 	}
 }
-
 func main() {
 	dates := []string{"2023-12-30", "2023-12-31", "2024-01-01"}
-	result := extractDates(dates)
+	result, err := ExtractDates(dates)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	fmt.Println(result)
 }
