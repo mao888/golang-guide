549b6e148578dcb16cc7aa9ba8be750b6216a6ee
huchao <2557523039@qq.com>
2024-02-21 16:08:24 +0800

    feat: project/业务/三方抓数/appier/appier

---
+++ project/业务/三方抓数/appier/appier.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	glog "github.com/mao888/mao-glog"
+	"gopkg.in/resty.v1"
+)
+
+type (
+	AppierRes struct {
+		Date         string  `json:"date"`
+		CampaignId   string  `json:"campaign_id"`
+		CampaignName string  `json:"campaign_name"`
+		Impressions  int     `json:"impressions"`
+		Clicks       int     `json:"clicks"`
+		Installs     int     `json:"installs"`
+		Cost         float32 `json:"cost"`
+		Currency     string  `json:"currency"`
+		Timezone     int     `json:"timezone"`
+	}
+)
+
+func main() {
+	ctx := context.Background()
+	var (
+		basicUrl    = "https://mmp.appier.org/campaign_report"
+		accessToken = "54f5a1241bc64e5fb60b678414f14929"
+		startDate   = "2024-01-25"
+		endDate     = "2024-01-25"
+		timezone    = -8
+	)
+	url := fmt.Sprintf("%s?access_token=%s&start_date=%s&end_date=%s&timezone=%d",
+		basicUrl, accessToken, startDate, endDate, timezone)
+	glog.Infof(ctx, "url:%s", url)
+
+	resp, err := resty.New().SetRetryCount(3).R().Get(url)
+	if err != nil {
+		glog.Errorf(ctx, "Get err:%s", err)
+		return
+	}
+	glog.Infof(ctx, "resp:%s", string(resp.Body()))
+
+	var appierRes []AppierRes
+	err = json.Unmarshal(resp.Body(), &appierRes)
+	if err != nil {
+		glog.Errorf(ctx, "Unmarshal err:%s", err)
+		return
+	}
+	glog.Infof(ctx, "appierRes:%v", appierRes)
+	glog.Infof(ctx, "appierRes的长度:%d", len(appierRes))
+}
