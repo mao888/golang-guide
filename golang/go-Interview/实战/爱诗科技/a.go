388636ceff7aecf0d397cd3720767aa37c508eca
huchao <2557523039@qq.com>
2024-07-25 14:53:49 +0800

    feat:爱诗科技

--- golang/go-Interview/实战/爱诗科技/a.go
+++ golang/go-Interview/实战/爱诗科技/a.go
@@ -6,11 +6,12 @@ package main
 //	Right *TreeNode
 //}
 
-//type ListNode struct {
-//	Val  int
-//	Next *ListNode
-//}
-
+//	type ListNode struct {
+//		Val  int
+//		Next *ListNode
+//	}
+//
+// m 个生产者，n 个消费者，用 channel 进行数据通信，消费函数为 func work（msg int） error，当消费捕获到 error 的时候，关闭所有的生产者和消费者。
 func main() {
 
 }
