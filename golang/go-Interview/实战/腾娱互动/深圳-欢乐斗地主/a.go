27c7852f09e7dda53652851c5fcd1bdd99568ee7
huchao <2557523039@qq.com>
2024-07-27 00:44:36 +0800

    feat:add 腾娱互动-欢乐斗地主

--- golang/go-Interview/实战/腾娱互动/深圳-欢乐斗地主/a.go
+++ golang/go-Interview/实战/腾娱互动/深圳-欢乐斗地主/a.go
@@ -5,7 +5,7 @@ import "fmt"
 func modifySlice(s []int) {
 	s[0] = 99
 	s = append(s, 4)
-	fmt.Println("Inside modifySlice:", s)
+	fmt.Println("Inside modifySlice:", s) // 99 2 3 4
 }
 
 func main() {
@@ -14,5 +14,5 @@ func main() {
 	slice = append(slice, 2)
 	slice = append(slice, 3)
 	modifySlice(slice)
-	fmt.Println("After modifySlice:", slice)
+	fmt.Println("After modifySlice:", slice) // 99 2 3
 }
