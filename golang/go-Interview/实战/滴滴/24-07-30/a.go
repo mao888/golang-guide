cafbb36c61c72a11d86df0f9c859291e439385d2
huchao <2557523039@qq.com>
2024-07-30 11:58:43 +0800

    feat:add 滴滴面试

--- golang/go-Interview/实战/滴滴/24-07-30/a.go
+++ golang/go-Interview/实战/滴滴/24-07-30/a.go
@@ -13,6 +13,31 @@ import "fmt"
 //	Next *ListNode
 //}
 
+//1. 给出一个长度非空的整形数组，元素值先递增后递减，找出数组中的最大值
+//eg: []int{1, 2, 3, 4, 6, 3, 1} 输出6
+//eg: []int{1, 2, 3, 4, 6} 输出6
+//eg: []int{6, 4, 3, 2, 1} 输出6
+
+func findMaxValue(arr []int) int {
+	left, right := 0, len(arr)-1
+	for left < right {
+		mid := left + (right-left)/2
+
+		// 如果 mid 指向的元素 > 右侧的，说明 max 在 mid 或其左侧
+		if arr[mid] > arr[mid+1] {
+			right = mid
+		} else {
+			// max 在 mid 右侧
+			left = mid + 1
+		}
+	}
+	// left == right 此时 max
+	return arr[left]
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+
+	fmt.Println(findMaxValue([]int{1, 2, 3, 4, 6, 3, 1}))
+	fmt.Println(findMaxValue([]int{1, 2, 3, 4, 6, 7}))
+	fmt.Println(findMaxValue([]int{7, 6, 4, 3, 2, 1}))
 }
