650f45fcbf986989fc185db942c286e6c0fbeec4
huchao <2557523039@qq.com>
2024-07-30 23:46:49 +0800

    feat:add滴滴面经

--- golang/go-Interview/实战/滴滴/24-07-30/a.go
+++ golang/go-Interview/实战/滴滴/24-07-30/a.go
@@ -18,6 +18,13 @@ import "fmt"
 //eg: []int{1, 2, 3, 4, 6} 输出6
 //eg: []int{6, 4, 3, 2, 1} 输出6
 
+// 初始化两个指针 left 和 right 分别指向数组的第一个元素和最后一个元素。
+// 进入循环，直到 left 指针小于 right 指针：
+// a. 计算中间指针 mid。
+// b. 如果 mid 指向的元素大于其右侧的元素，说明最大值可能在 mid 或其左侧，因此将 right 指针移动到 mid。
+// c. 否则，说明最大值可能在 mid 的右侧（但不在 mid 本身，因为数组是先递增后递减的），因此将 left 指针移动到 mid + 1。
+// 当循环结束时，left 和 right 指针会相遇，它们指向的元素就是数组中的最大值。
+// 这个算法的时间复杂度是 O(logn)，因为它每次都将搜索范围减半，直到找到最大值。
 func findMaxValue(arr []int) int {
 	left, right := 0, len(arr)-1
 	for left < right {
