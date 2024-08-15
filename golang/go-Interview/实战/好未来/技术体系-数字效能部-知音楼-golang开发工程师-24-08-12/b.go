6d75b8b3eb1f84afd67edf3871c69ce1b3806eb6
huchao <2557523039@qq.com>
2024-08-15 18:40:07 +0800

    feat:add 好未来二面

--- golang/go-Interview/实战/好未来/技术体系-数字效能部-知音楼-golang开发工程师-24-08-12/b.go
+++ golang/go-Interview/实战/好未来/技术体系-数字效能部-知音楼-golang开发工程师-24-08-12/b.go
@@ -1,31 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-//type ListNode struct {
-//	Val  int
-//	Next *ListNode
-//}
+//求子数组的最大和
+//题目:输入一个整形数组，数组里有正数也有负数。 数组中连续的一个或多个整数组成一个子数组，每个子数组都有一个和。 求所有子数组的和的最大值。要求时间复杂度为 O(n)。
+//例如输入的数组为1, -2, 3, 10, -4, 7, 2, -5，和最大的子数组为3, 10, -4, 7, 2， 因此输出为该子数组的和18。
 
-//type TreeNode struct {
-//	Val   int
-//	Left  *TreeNode
-//	Right *TreeNode
-//}
-// 构建一个示例二叉树
-//        1
-//       / \
-//      2   3
-//     /|   |\
-//    4 5   6 7
-//root := &TreeNode{Val: 1}
-//root.Left = &TreeNode{Val: 2}
-//root.Right = &TreeNode{Val: 3}
-//root.Left.Left = &TreeNode{Val: 4}
-//root.Left.Right = &TreeNode{Val: 5}
-//root.Right.Left = &TreeNode{Val: 6}
-//root.Right.Right = &TreeNode{Val: 7}
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	localMax := nums[0]
+	globalMax := nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		// 判断是开始一个新的子数组还是从上一个子数组中继续
+		localMax = max(nums[i], localMax+nums[i])
+		// 更新全局最大和
+		globalMax = max(globalMax, localMax)
+	}
+	return globalMax
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	nums := []int{1, -2, 3, 10, -4, 7, 2, -5}
+	fmt.Println("最大子数组和为:", maxSubArray(nums))
 }
