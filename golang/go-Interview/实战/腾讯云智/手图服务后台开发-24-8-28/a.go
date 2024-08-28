300d5801669fb67cedc203a9bfbc38047aa60a2c
huchao <2557523039@qq.com>
2024-08-28 09:25:48 +0800

    feat:add 腾讯云智面经

--- golang/go-Interview/实战/腾讯云智/手图服务后台开发-24-8-28/a.go
+++ golang/go-Interview/实战/腾讯云智/手图服务后台开发-24-8-28/a.go
@@ -1,53 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
-// golang实现3个goroutine交替打印1~100。例如：
-// Goroutine 1: 1
-// Goroutine 2: 2
-// Goroutine 3: 3
-// Goroutine 1: 4
-// Goroutine 2: 5
-// Goroutine 3: 6
-// Goroutine 1: 7
-// Goroutine 2: 8
-// Goroutine 3: 9
-// Goroutine 1: 10
-// Goroutine 2: 11
-// Goroutine 3: 12
+//type TreeNode struct {
+//	Val   int
+//	Left  *TreeNode
+//	Right *TreeNode
+//}
+
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
+
 func main() {
-	var wg sync.WaitGroup
-	printChan := make(chan int, 1)
-
-	goroutines := 3
-	totalNumbers := 100
-
-	wg.Add(goroutines)
-	for i := 0; i < goroutines; i++ {
-		go func(id int) {
-			defer wg.Done()
-			for {
-				num, ok := <-printChan
-				if !ok {
-					return
-				}
-				if num > totalNumbers {
-					close(printChan)
-					return
-				}
-				if (num-1)%goroutines == id {
-					fmt.Printf("Goroutine %d: %d\n", id+1, num)
-					printChan <- num + 1
-				} else {
-					printChan <- num
-				}
-			}
-		}(i)
-	}
-
-	printChan <- 1
-	wg.Wait()
+	fmt.Println("hello world")
 }
