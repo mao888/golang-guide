1438cfc54d71da0e9c400bbb4da1d872047f87e0
huchao <2557523039@qq.com>
2024-09-03 00:32:31 +0800

    feat:add百度面经

--- golang/go-Interview/实战/百度/推荐产品研发组_GOPHP研发工程师-8-29/a.go
+++ golang/go-Interview/实战/百度/推荐产品研发组_GOPHP研发工程师-8-29/a.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 //一个n米深的井，每分钟只能爬u米，每次爬之前都要休息一分钟，休息期间会下滑d米，求爬出井需要多少分钟？
 //输入n，u，d，输出所需分钟
@@ -18,9 +21,27 @@ func minute(n, u, d int) int {
 	return total
 }
 
+func calculateMinutes(n, u, d int) int {
+	// 每次实际上升的高度
+	actualClimb := u - d
+	// 除最后一次外，需要爬升的次数
+	climbsNeeded := math.Ceil(float64(n-u) / float64(actualClimb))
+	// 总分钟数：爬升次数*2（每次爬升和休息）+ 最后一次爬升
+	totalMinutes := int(climbsNeeded*2) + 1
+	return totalMinutes
+}
+
 func main() {
 	n := 10
 	u := 2
 	d := 1
 	fmt.Println(minute(n, u, d))
+	fmt.Println(calculateMinutes(n, u, d))
+
+	fmt.Println("--------------------------------")
+	n = 10
+	u = 3
+	d = 1
+	fmt.Println(minute(n, u, d))
+	fmt.Println(calculateMinutes(n, u, d))
 }
