5e27b14f871581f0d1db0d939bd723e8710ee833
huchao <2557523039@qq.com>
2024-09-29 16:08:47 +0800

    feat:add 百度电商

--- golang/go-Interview/实战/百度/百度电商-24-9-29/a.go
+++ golang/go-Interview/实战/百度/百度电商-24-9-29/a.go
@@ -1,31 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-//type ListNode struct {
-//	Val  int
-//	Next *ListNode
-//}
+//找零36，现有硬币面额1、2、5、10、
+//20（各面值硬币量足够）：最少需要
+//多少枚硬币，该硬币组合是？
 
-//type TreeNode struct {
-//	Val   int
-//	Left  *TreeNode
-//	Right *TreeNode
-//}
-// 构建一个示例二叉树
-//        1
-//       / \
-//      2   3
-//     /|   |\
-//    4 5   6 7
-//root := &TreeNode{Val: 1}
-//root.Left = &TreeNode{Val: 2}
-//root.Right = &TreeNode{Val: 3}
-//root.Left.Left = &TreeNode{Val: 4}
-//root.Left.Right = &TreeNode{Val: 5}
-//root.Right.Left = &TreeNode{Val: 6}
-//root.Right.Right = &TreeNode{Val: 7}
+// 找零问题：给定面额为1、2、5、10、20的硬币，找零36最少需要多少枚硬币
+func coinChange(coins []int, amount int) (int, []int) {
+	// 建立一个长度为 amount+1 的数组，用于存储每个金额所需的最少硬币数
+	dp := make([]int, amount+1)
+	// 初始化 dp 数组，将未被计算过的金额设为 math.MaxInt32
+	for i := 1; i <= amount; i++ {
+		dp[i] = math.MaxInt32
+	}
+
+	// 用于记录每个金额所使用的硬币
+	coinUsed := make([][]int, amount+1)
+
+	// 动态规划过程
+	for i := 1; i <= amount; i++ {
+		for _, coin := range coins {
+			if i-coin >= 0 && dp[i-coin]+1 < dp[i] {
+				dp[i] = dp[i-coin] + 1
+				// 复制前一个状态的硬币使用记录，并添加当前硬币
+				coinUsed[i] = append(coinUsed[i-coin], coin)
+			}
+		}
+	}
+
+	// 如果 dp[amount] 仍然是 math.MaxInt32，说明无法找零
+	if dp[amount] == math.MaxInt32 {
+		return -1, nil
+	}
+
+	return dp[amount], coinUsed[amount]
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	coins := []int{1, 2, 5, 10, 20}
+	amount := 36
+
+	minCoins, coinCombination := coinChange(coins, amount)
+	if minCoins == -1 {
+		fmt.Println("无法找零")
+	} else {
+		fmt.Printf("最少需要 %d 枚硬币\n", minCoins)
+		fmt.Printf("硬币组合是: %v\n", coinCombination)
+	}
 }
